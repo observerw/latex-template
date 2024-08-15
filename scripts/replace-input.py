9bc1eefd2270e025ee5211cb1af52d33cdef17f8
observer <wozluohd@gmail.com>
2024-08-16 00:08:52 +0800

    feat: add ignore-comment for replace-input.py

--- scripts/replace-input.py
+++ scripts/replace-input.py
@@ -17,53 +17,48 @@ this will work, but content after \\input will be ignored:
 """
 
 import argparse
-from pathlib import Path
 import re
 import sys
+from pathlib import Path
 from typing import Iterable
 
 tex_input_re = re.compile(r"^\s*\\input{(.*?)}")
 tex_include_re = re.compile(r"^\s*\\include{(.*?)}")
 
 
-def transform_line(base_path: Path, line: str) -> Iterable[str]:
-    match = tex_input_re.match(line) or tex_include_re.match(line)
-    if not match:
-        yield line
-        return
+def transform(
+    base_path: Path,
+    input_path: Path,
+    ignore_comment: bool = False,
+) -> Iterable[str]:
+    def transform_line(base_path: Path, line: str) -> Iterable[str]:
+        match = tex_input_re.match(line) or tex_include_re.match(line)
+        if not match:
+            yield line
+            return
 
-    include_path = match.group(1)
-    if not include_path or not include_path.endswith(".tex"):
-        raise ValueError(f"Invalid include path: {include_path}")
+        include_path = match.group(1)
+        if not include_path or not include_path.endswith(".tex"):
+            raise ValueError(f"Invalid include path: {include_path}")
 
-    include_path = Path(base_path / include_path)
-    if not include_path.exists():
-        raise FileNotFoundError(f"File {include_path} not found")
+        include_path = Path(base_path / include_path)
+        if not include_path.exists():
+            raise FileNotFoundError(f"File {include_path} not found")
 
-    yield "\n"
-    for transformed_line in transform(base_path, include_path):
-        yield transformed_line
-    yield "\n"
+        yield "\n"
+        for transformed_line in transform(base_path, include_path):
+            if ignore_comment and transformed_line.strip().startswith("%"):
+                continue
 
-def transform(base_path: Path, input_path: Path) -> Iterable[str]:
+            yield transformed_line
+        yield "\n"
+
     with open(input_path, "r", encoding="utf-8") as f:
-        for line in f: 
+        for line in f:
             yield from transform_line(base_path, line)
 
 
-def main(base_path: Path, input_path: Path, output_path: Path):
-    with open(output_path, "w", encoding="utf-8") as f:
-        f.writelines(transform(base_path, input_path))
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", help="input file")
-    parser.add_argument("-o", "--output", help="output file")
-    parser.add_argument("-b", "--base_path", help="base path for files")
-
-    args = parser.parse_args()
-
+def main(args: argparse.Namespace):
     base_path = Path(args.base_path)
     input_path = Path(base_path / args.input)
     output_path = Path(base_path / args.output)
@@ -71,10 +66,30 @@ if __name__ == "__main__":
     if not input_path.exists():
         raise FileNotFoundError(f"File {input_path} not found")
 
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.writelines(
+            transform(
+                base_path,
+                input_path,
+                ignore_comment=args.ignore_comment,
+
+            )
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="input file")
+    parser.add_argument("-o", "--output", help="output file")
+    parser.add_argument("-b", "--base_path", help="base path for files")
+    parser.add_argument("--ignore-comment", help="ignore comments", action="store_true")
+
+    args = parser.parse_args()
+
     try:
-        main(base_path, input_path, output_path)
+        main(args)
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
         exit(1)
 
-    print(f"File {input_path} transformed to {output_path}")
+    print(f"File {args.input_path} transformed to {args.output_path}")
